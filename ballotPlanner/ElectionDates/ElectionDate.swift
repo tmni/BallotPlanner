36f748acbddd8ccf84273a2f54d9c0f1b7af29e1
Erika Giuse <erikagiuse@gmail.com>
2019-11-03 17:42:23 -0500

    Added Firebase

---
+++ ballotPlanner/ElectionDates/ElectionDate.swift
@@ -0,0 +1,13 @@
+//
+//  Election.swift
+//  ballotPlanner
+//
+//  Created by Erika Giuse on 11/3/19.
+//  Copyright © 2019 Erika Giuse. All rights reserved.
+//
+
+struct ElectionDate {
+  var date: String
+  var description: String
+  var name: String
+}
---
+++ ballotPlanner/ElectionDates/ElectionDateDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ElectionDateDetailViewModel.swift
+//  ballotPlanner
+//
+//  Created by Erika Giuse on 11/3/19.
+//  Copyright © 2019 Erika Giuse. All rights reserved.
+//
+
+import Foundation
