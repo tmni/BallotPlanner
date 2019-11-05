38f1be38c69db7bdcd33013496afdf2c74e79bd9
Erika Giuse <erikagiuse@gmail.com>
2019-11-05 16:45:07 -0500

    merged taruna stuff

--- ballotPlanner/AppDelegate.swift
+++ ballotPlanner/AppDelegate.swift
@@ -14,10 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
+  override init() {
+    super.init()
+    FirebaseApp.configure()
+  }
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
-    FirebaseApp.configure()
+    //FirebaseApp.configure()
     return true
   }
 
