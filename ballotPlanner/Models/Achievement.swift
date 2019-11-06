dea6c770e36327dea33ccd866e5410d4f58cf945
Taruna Emani <temani@andrew.cmu.edu>
2019-11-06 14:21:48 -0500

    made a basic collection, translating to achievements

---
+++ ballotPlanner/Models/Achievement.swift
@@ -0,0 +1,17 @@
+//
+//  Achievement.swift
+//  
+//
+//  Created by Taruna Emani on 11/6/19.
+//
+
+import Foundation
+import UIKit
+
+
+struct Achievement{
+  var title: String
+  var icon: UIImage?
+  var isActive: Bool
+  
+}
---
+++ ballotPlanner/User Info/AchievementsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AchievementsViewController.swift
+//  ballotPlanner
+//
+//  Created by Taruna Emani on 11/6/19.
+//  Copyright © 2019 Erika Giuse. All rights reserved.
+//
+
+import UIKit
+
+final class AchievementsViewController: UICollectionViewController {
+  // MARK: - Properties
+  private let reuseIdentifier = "achievement"
+  private let sectionInsets = UIEdgeInsets(top: 50.0,
+                                           left: 20.0,
+                                           bottom: 50.0,
+                                           right: 20.0)
+
+  
+}
