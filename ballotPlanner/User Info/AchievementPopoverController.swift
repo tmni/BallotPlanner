d609054279b689233edc6ced3acf6ed8c647c3e6
Taruna Emani <temani@andrew.cmu.edu>
2019-11-23 14:56:00 -0500

    added popovers for achievements

---
+++ ballotPlanner/User Info/AchievementPopoverController.swift
@@ -0,0 +1,35 @@
+//
+//  AchievementPopoverController.swift
+//  ballotPlanner
+//
+//  Created by Taruna Emani on 11/23/19.
+//  Copyright © 2019 Erika Giuse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class AchievementPopoverController: UIViewController {
+  
+    @IBOutlet weak var achTitleUI: UILabel!
+    @IBOutlet weak var achDescUI: UILabel!
+  
+  
+  
+    var achTitle: String!
+    var achDesc: String!
+    
+    var selectedVideoTitle: String!
+    var message: String!
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      achTitleUI.text = achTitle
+      achDescUI.text = achDesc
+    }
+    
+    override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+    }
+    
+}
