a3a7bc28b05e291693e3cc76855fda16fd87b792
Taruna Emani <temani@andrew.cmu.edu>
2019-12-11 18:54:58 -0500

    styled candidate index

--- ballotPlanner/Elections/Candidates Index/CandidatesIndexCollectionViewCell.swift
+++ ballotPlanner/Elections/Candidates Index/CandidatesIndexCollectionViewCell.swift
@@ -13,6 +13,17 @@ class CandidatesIndexCollectionViewCell: UICollectionViewCell {
   @IBOutlet weak var personImage: UIImageView!
   @IBOutlet weak var name: UILabel!
   @IBOutlet weak var party: UILabel!
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder:aDecoder)
+    //You Code here
+    layer.shadowColor = UIColor.gray.cgColor
+    layer.shadowOffset = CGSize(width: 0, height: 2.0)
+    layer.shadowRadius = 1.0
+    layer.shadowOpacity = 0.4
+    layer.masksToBounds = false
+   
+  }
+  
   
   override func awakeFromNib() {
       super.awakeFromNib()
